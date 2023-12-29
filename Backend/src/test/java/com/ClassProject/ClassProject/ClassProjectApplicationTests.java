2ff57282d70c0f43a8ecaae9735ce9bf21b03467
Kartik Sharma <kash5740@colorado.edu>
2023-12-29 08:40:53 -0700

    added spring boot backend boiler plate with dependicies

---
+++ Backend/src/test/java/com/ClassProject/ClassProject/ClassProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClassProject.ClassProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
